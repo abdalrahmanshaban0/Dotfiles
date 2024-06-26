a8c8883a042ec34ce9ae347854061b9907d492f1
Abdalrahman Shaban <abdalrahmanshaban52@gmail.com>
2024-06-26 19:26:39 +0300

    Add files via upload

--- dwm/config.h
+++ dwm/config.h
@@ -2,7 +2,7 @@
 
 /* appearance */
 static const unsigned int borderpx  = 2;        /* border pixel of windows */
-static const unsigned int gappx     = 5;        /* gaps between windows */
+static const unsigned int gappx     = 10;        /* gaps between windows */
 static const unsigned int snap      = 35;       /* snap pixel */
 static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
 static const unsigned int systrayonleft = 0;    /* 0: systray in the right corner, >0: systray on left of status text */
@@ -11,7 +11,7 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;        /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "HackNerdFontMono:size=9" };
+static const char *fonts[]          = { "HackNerdFontMono:size=10" };
 static const char dmenufont[]       = "HackNerdFontMono:size=10";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
@@ -31,11 +31,14 @@ static const char *const autostart[] = {
 	/*NULL  terminate */
 	"sh",	"-c",	"brightnessctl set 40",	NULL,
 	"sh",	"-c",	"picom",	NULL,
+	"sh",	"-c",	"setxkbmap -layout us,eg -option grp:alt_shift_toggle",	NULL,
+	"sh",	"-c",	"/home/abdalrahman/.src/Islamic-Prayer-Timings/update_timings.sh",	NULL,
+	"sh",	"-c",	"/home/abdalrahman/.src/Islamic-Prayer-Timings/update_rem.sh",	NULL,
+	"sh",	"-c",	"xrandr --output DP-1-2 --mode 1920x1080 --rate 74.97 --left-of eDP-1 &",	NULL,
 	"sh",	"-c",	"sxhkd",	NULL,
 	"sh",	"-c",	"slstatus",	NULL,
 	"sh",	"-c",	"nm-applet",	NULL,
 	"sh",	"-c",	"~/.fehbg",	NULL,
-	"sh",	"-c",	"xinput set-prop \"10\" \"libinput Accel Profile Enabled\" 0 1 0",	NULL,
 };
 
 
@@ -67,7 +70,7 @@ static const Layout layouts[] = {
 };
 
 /* key definitions */
-#define MODKEY Mod1Mask
+#define MODKEY Mod4Mask
 #define TAGKEYS(KEY,TAG) \
 	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
@@ -80,12 +83,12 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "st", NULL };
+static const char *termcmd[]  = { "alacritty", NULL };
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
-	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
@@ -93,9 +96,9 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
-	{ MODKEY,                       XK_Return, zoom,           {0} },
+	{ MODKEY|ShiftMask,                       XK_Return, zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
-	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
+	{ MODKEY|ShiftMask,             XK_q,      killclient,     {0} },
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
 	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
@@ -121,7 +124,7 @@ static const Key keys[] = {
 	TAGKEYS(                        XK_7,                      6)
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
-	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+	{ MODKEY|ShiftMask|ControlMask,  XK_q,      quit,           {0} },
 };
 
 /* button definitions */
